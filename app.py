1dd0023d18154c6590283f0b2a54d8cfd14a6141
“javismiles” <ideami@gmail.com>
2020-03-01 02:31:02 +0100

    prediction code

--- app.py
+++ app.py
@@ -33,19 +33,23 @@ def predict():
     
         labels = ['grizzly','black','teddy']
         #Collecting values from the html form and converting into respective types as expected by the model
-        #Brand =  request.form["Brand"]
+
+        file = request.files['file']
+
+        if file:
+            filename = file.filename
+            file.save(os.path.join("resources/bears", filename))
 
         #fastai predicts from a pandas series. so converting the list to a series
-        to_predict = "resources/bears/teddy/images586.jpg"
-        to_predict = "resources/bears/black/Black20Bear206402.jpg"
+        #to_predict = "resources/bears/teddy/images586.jpg"
+        to_predict = "resources/bears/"+filename
 
         #Getting the prediction from the model and rounding the float into 2 decimal places
         prediction=model.predict(to_predict)
 
-        print("result: ",prediction[0])
+        #print("result: ",prediction[0])
 
-
-        #return render_template('index.html', prediction_text='Your Prediction :  {} '.format(prediction))
+        return render_template('index.html', prediction_text='Your Prediction :  {} '.format(prediction[0]))
 
 
 if __name__ == "__main__":
