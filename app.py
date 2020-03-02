1b432ce1e5bb02e665690c17c6e4b5f3d98c74af
“javismiles” <ideami@gmail.com>
2020-03-03 00:50:29 +0100

    update app

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+##############################################
+##############################################
+###### Predict the Bear ######################
+# Flask app that uses a model trained with the Fast.ai v2 library
+# following an example in the upcoming book "Deep Learning for Coders
+# with fastai and PyTorch: AI Applications Without a PhD" by
+# Jeremy Howard and Sylvain Gugger.
+##############################################
+# Project put together by Javier Ideami
+# Email: ideami@ideami.com
+# Web: ideami.com
+##############################################
+
 import numpy as np
 from flask import Flask, request, render_template
 import pickle
@@ -8,7 +21,6 @@ from fastai2.imports import *
 
 import os
 
-
 cwd = os.getcwd()
 path = Path()
 Path().ls(file_exts='.pkl')
@@ -16,8 +28,6 @@ Path().ls(file_exts='.pkl')
 application = Flask(__name__)
 
 model = load_learner(path/'model/export.pkl')
-
-
 
 #Defining the home page for the web service
 @application.route('/')
@@ -27,31 +37,27 @@ def home():
 #Writing api for inference using the loaded model
 @application.route('/predict',methods=['POST'])
 
-#Defining the predict method get input from the html page and to predict using the trained model
-
+#Predict method that uses the trained model to predict the kind of bear in the picture we uploaded
 def predict():
     
-        labels = ['grizzly','black','teddy']
-        #Collecting values from the html form and converting into respective types as expected by the model
+        #labels = ['grizzly','black','teddy']
 
         file = request.files['file']
 
+        #Store the uploaded images in a temporary folder
         if file:
             filename = file.filename
             file.save(os.path.join("resources/tmp", filename))
 
-        #fastai predicts from a pandas series. so converting the list to a series
-        #to_predict = "resources/bears/teddy/images586.jpg"
         to_predict = "resources/tmp/"+filename
 
-        #Getting the prediction from the model and rounding the float into 2 decimal places
+        #Getting the prediction from the model
         prediction=model.predict(to_predict)
 
-        #print("result: ",prediction[0])
-
+        #Render the result in the html template
         return render_template('index.html', prediction_text='Your Prediction :  {} '.format(prediction[0]))
 
 
 if __name__ == "__main__":
-    #predict()
+    #run the application
     application.run(host='0.0.0.0')
